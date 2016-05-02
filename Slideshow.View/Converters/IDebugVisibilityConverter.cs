5d421359852b76db71987b46cc6ea1fe857315fc
Alex Forbes-Reed <alexreed@outlook.com>
2016-05-02 16:09:44 +0100

    added a debug visibility filter

---
+++ Slideshow.View/Converters/IDebugVisibilityConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Slideshow.View.Converters
+{
+	public class IDebugVisibilityConverter
+		: IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+#if DEBUG
+			return Visibility.Visible;
+#else
+			return Visibility.Collapsed;
+#endif
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
